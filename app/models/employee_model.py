801afa9d2aa0e0f939d6a471d5b2b54110f58bd3
dvdm12 <dvddavid677@gmail.com>
2024-09-07 13:15:20 -0500

    i've established a db connection through alembic and created tables in bd

--- app/models/employee_model.py
+++ app/models/employee_model.py
@@ -7,8 +7,8 @@ class Employee(Base):
     __tablename__ = "employees"
 
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, index=True)
-    last_name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
+    first_name = Column(String(50), index=True)
+    last_name = Column(String(50), index=True)
+    email = Column(String(100), unique=True, index=True)
 
     tasks = relationship("Task", back_populates="employee")
