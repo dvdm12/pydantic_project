5a42751011753b3f4a1fc9c17745d053a732b184
dvdm12 <dvddavid677@gmail.com>
2024-09-07 13:42:49 -0500

    requeriments.txt has been uploaded

--- app/models/employee_model.py
+++ app/models/employee_model.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from app.config.config import Base
+from app.schemas.task_schema import Task
 
 
 class Employee(Base):
