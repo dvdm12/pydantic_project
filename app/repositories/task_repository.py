fb366fce5dcdc4cc8adbec320635daf5f2ae3a4b
dvdm12 <dvddavid677@gmail.com>
2024-09-07 11:18:49 -0500

    i've added delete functions on task_repository and service

--- app/repositories/task_repository.py
+++ app/repositories/task_repository.py
@@ -40,3 +40,9 @@ def get_task_by_id(db: Session, task_id: int):
     return db.query(Task).filter(Task.id == task_id).first()
 
 
+def delete_task(db: Session, task_id: int):
+    task = db.query(Task).filter(Task.id == task_id).first()
+    if task:
+        db.delete(task)
+        db.commit()
+    return task
