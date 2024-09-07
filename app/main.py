7ba5d30294ee622176338eb20703a583f2be7c45
dvdm12 <dvddavid677@gmail.com>
2024-09-07 11:07:13 -0500

    employee routes has added

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from app.models import employee_model, task_model
-
-
+from app.routes import employee_routes
 
 # FastAPI instance
 app = FastAPI(
@@ -9,3 +8,6 @@ app = FastAPI(
     description="API for managing employees and tasks using FastAPI, MariaDb, SQLAlchemy, and Alembic.",
     version="1.0.0"
 )
+
+# Registrar las rutas
+app.include_router(employee_routes.router, prefix="/api/v1", tags=["Employees"])
